a4d7aba83e0e3e0fb599c99a5d782520b911e7f4
chudymikolaj <chudymikolaj06@gmail.com>
2024-06-20 21:42:40 +0200

    feat: create LinkWithIcon component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,18 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface LinkLinkWithIcon extends Schema.Component {
+  collectionName: 'components_link_link_with_icons';
+  info: {
+    displayName: 'LinkWithIcon';
+    icon: 'link';
+  };
+  attributes: {
+    Name: Attribute.String;
+    Link: Attribute.String;
+    Icon: Attribute.Media<'images'>;
+  };
+}
+
 export interface LinkLink extends Schema.Component {
   collectionName: 'components_link_links';
   info: {
@@ -15,6 +28,7 @@ export interface LinkLink extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'link.link-with-icon': LinkLinkWithIcon;
       'link.link': LinkLink;
     }
   }
